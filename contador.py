25e5ce28df826d5bc4462353cfbd78716159a01c
Felipe Matheus de Souza <felipemds86@hotmail.com>
2022-09-14 17:05:12 -0300

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,7 @@
+contador = 0
+
+x=int(input("digite um valor para x:"))
+while (x>0):
+    contador = contador +1
+    x=int(input("digite um valor para x: "))
+print("Foram digitados ", contador , "numeros")
---
+++ contador_ for.py
@@ -0,0 +1,11 @@
+print("Termos da sequencia")
+
+num= int(input("digite o numero de termos da sequencia"))
+a = 1
+b = aux = 2
+print( a, end = ' ' )
+for contador in range (num -1):
+         print (aux , end = (' ' )
+        aux = a * b
+         a = b
+         b = aux 
